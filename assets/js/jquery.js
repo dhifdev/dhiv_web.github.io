de339c8a7d72765af467bd39466fa6ace75d669f
DivDev <112265867+dhifdev@users.noreply.github.com>
2022-10-15 12:06:41 +0700

    Tugas p07SPA Adhivia Julian NIM : 41215568

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
   $(".opsi").click(function () {
     let id = $(this).prop("id");
     let split = id.split("_");
-    let nu_aing = split[1];
+    let my_me = split[1];
 
     $(".opsi").removeClass("aktif");
-    $("#isi_" + nu_aing).addClass("aktif");
+    $("#isi_" + my_me).addClass("aktif");
     $(".home").slideUp();
     $("#konten_1").slideUp();
     $("#konten_2").slideUp();
     $("#konten_3").slideUp();
-    $("#konten_" + nu_aing).slideToggle();
+    $("#konten_" + my_me).slideToggle();
   });
 });
